0cb46b87bbf986ced3a0a0a74a8b31eeafde50bc
Felix Hoehle <hoehle@physik.rwth-aachen.de>
2013-11-06 15:16:55 +0100

    LHE2EDM step added

---
+++ TT_mcatnlo_LHE2EDM_cfg.py
@@ -0,0 +1,62 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303.2.7 
+# Source: /cvs/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: LHE2EDM -s NONE --conditions auto:startup --pileup NoPileUp --datamix NODATAMIXER --eventcontent RAWSIM --datatier GEN-SIM --filein file:../MCatNLO/TT_mcatnlo.lhe.root --fileout TT_mcatnlo_LHE2EDM.root --python_filename TT_mcatnlo_LHE2EDM_cfg.py --no_exec -n -1 --filetype LHE
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('LHE')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+# Input source
+process.source = cms.Source("LHESource",
+    fileNames = cms.untracked.vstring('file:../MCatNLO/TT_mcatnlo.lhe.root')
+)
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 1.303.2.7 $'),
+    annotation = cms.untracked.string('LHE2EDM nevts:-1'),
+    name = cms.untracked.string('PyReleaseValidation')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('TT_mcatnlo_LHE2EDM.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+process.GlobalTag.globaltag = 'START42_V12::All'
+
+# Path and EndPath definitions
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.RAWSIMoutput_step)
